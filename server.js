061d32bc95654640b8f743b38df2e3adbabc4827
memonyasin <35967723+memonyasin@users.noreply.github.com>
2018-02-24 19:36:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get("/article-one",function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
 }); 
 app.get("/artical-two",function(req,res){
     res.send("yasin memon2");
